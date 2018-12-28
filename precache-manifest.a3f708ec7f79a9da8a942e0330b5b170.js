020ee3a42d9ac06e71e396ae2353274e6148598b
krzyzowskamartyna <krzyzowskamartyna@gmail.com>
2018-12-28 14:36:42 +0100

    Updates

---
+++ precache-manifest.a3f708ec7f79a9da8a942e0330b5b170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ee90fef419b0337d269",
+    "url": "/Weather-app/static/js/runtime~main.6ee90fef.js"
+  },
+  {
+    "revision": "11e080eb1d3580c24fe1",
+    "url": "/Weather-app/static/js/main.11e080eb.chunk.js"
+  },
+  {
+    "revision": "d6c6434880bf5f87ab42",
+    "url": "/Weather-app/static/js/1.d6c64348.chunk.js"
+  },
+  {
+    "revision": "11e080eb1d3580c24fe1",
+    "url": "/Weather-app/static/css/main.b3363b2a.chunk.css"
+  },
+  {
+    "revision": "d6c6434880bf5f87ab42",
+    "url": "/Weather-app/static/css/1.efcdb334.chunk.css"
+  },
+  {
+    "revision": "6ca313805b19a0ec8ec1a3f6ae5204c9",
+    "url": "/Weather-app/index.html"
+  }
+];
